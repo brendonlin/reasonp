71922ae02c30973b7120b9ebe053566aa5fe82a6
brendonlin <brendon.lin@outlook.com>
2022-03-06 20:10:33 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="reasonp",
-    version="0.1.1",
+    version="0.1.2",
     author="brendon.lin",
     author_email="brendon.lin@outlook.com",
     packages=find_packages(exclude=["tests"]),
